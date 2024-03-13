9ca1c3fcaa512e578574449fc753068fa6a3f384
Elizaveta39 <2elizaveta8@gmail.com>
2024-03-13 16:50:19 +0400

    Work with matrix

--- matrix/input_columns.py
+++ matrix/input_columns.py
@@ -1,18 +1,18 @@
 # The task is to print a matrix filled with numbers from 1 to m * n
+# in such order: 1st column from up to down, 2nd from up to down, etc...
 # You will get n and m — the number of rows and columns in the matrix
 
 n, m = [int(i) for i in input().split()]
-matrix = []
+matrix = [[0] * m for _ in range(n)]
 
 elem = 1    # variable for values in matrix
 
-for i in range(n):
-    matrix.append([])
-    for j in range(m):
-        matrix[i].append(elem)
+for i in range(m):
+    for j in range(n):  # filling the matrix with numbers from 1 to m * n
+        matrix[j][i] = elem
         elem += 1
 
-# printing the matrix with 3 symbols for each matrix value
+# printing the matrix in correct order
 for row in range(n):
     for col in range(m):
         print(str(matrix[row][col]).ljust(3), end=" ")
